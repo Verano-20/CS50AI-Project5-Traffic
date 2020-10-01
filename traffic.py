559cf48f5d7735719c5731d8cefeafdfd84ac9f7
Verano-20 <samdawson97@hotmail.co.uk>
2020-10-01 18:12:20 +0100

    Submitted solution

--- traffic.py
+++ traffic.py
@@ -3,25 +3,21 @@ import numpy as np
 import os
 import sys
 import tensorflow as tf
+
 from sklearn.model_selection import train_test_split
-import pydot as pydot
-import csv
 
 EPOCHS = 10
 IMG_WIDTH = 30
 IMG_HEIGHT = 30
 NUM_CATEGORIES = 43
 TEST_SIZE = 0.4
-REPS = 10
+
 
 def main():
 
     # Check command-line arguments
     if len(sys.argv) not in [2, 3]:
         sys.exit("Usage: python traffic.py data_directory [model.h5]")
-
-    # Check GPUs
-    print(f"\nNum GPUs Available: {len(tf.config.experimental.list_physical_devices('GPU'))} \n")
 
     # Get image arrays and labels for all image files
     images, labels = load_data(sys.argv[1])
@@ -35,38 +31,12 @@ def main():
     # Get a compiled neural network
     model = get_model()
 
-    # plot picture of network
-    tf.keras.utils.plot_model(model, to_file="model.png", show_shapes=True, show_layer_names=False, rankdir="LR", expand_nested=False, dpi=200)
+    # Fit model on training data
+    model.fit(x_train, y_train, epochs=EPOCHS)
 
-    av_acc = 0
-    av_loss = 0     
-    count = 0
-    while count < REPS:
-        # Fit model on training data
-        model.fit(x_train, y_train, epochs=EPOCHS)
+    # Evaluate neural network performance
+    model.evaluate(x_test,  y_test, verbose=2)
 
-        # Evaluate neural network performance
-        evaluation = model.evaluate(x_test,  y_test, verbose=2)
-
-        av_acc += evaluation[1]
-        av_loss += evaluation[0]
-
-        count += 1
-
-    av_acc = av_acc / REPS
-    av_loss = av_loss / REPS
-
-    # get results and network structure
-    outputs = [av_acc, av_loss]
-    for layer in model.layers:
-        outputs.append((layer.name, tuple(layer.output.shape[1:])))
-
-    # Save evaluation and structure to csv
-    with open("optimise.csv", 'a', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(outputs)
-    file.close()
-  
     # Save model to file
     if len(sys.argv) == 3:
         filename = sys.argv[2]
@@ -111,26 +81,19 @@ def get_model():
     model = tf.keras.models.Sequential()
 
     # Define input shape
-    model.add(tf.keras.Input(shape = (30, 30, 3)))
+    model.add(tf.keras.Input(shape = (IMG_WIDTH, IMG_HEIGHT, 3)))
 
     # Convolution layer
-    model.add(tf.keras.layers.Conv2D(16, (CONV, CONV), activation="relu"))
+    model.add(tf.keras.layers.Conv2D(16, (3, 3), activation="relu"))
     
     # Max Pooling 2D layer
-    model.add(tf.keras.layers.MaxPooling2D(pool_size=(POOL, POOL)))
-    """
-    # Convolution layer
-    model.add(tf.keras.layers.Conv2D(32, (CONV, CONV), activation="relu"))
-    
-    # Max Pooling 2D layer
-    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
-    """
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(6, 6)))
+
     # Flatten Data to 1D
     model.add(tf.keras.layers.Flatten(name="flattened"))
-    flattened_units = model.get_layer("flattened").output.shape[1]
-    
-    for i in range(1):
-        model.add(tf.keras.layers.Dense(UNITS, activation="sigmoid"))
+
+    # Single Dense layer with 190 units
+    model.add(tf.keras.layers.Dense(190, activation="sigmoid"))
     
     # Output layer with NUM_CATEGORIES outputs
     model.add(tf.keras.layers.Dense(NUM_CATEGORIES, activation="sigmoid"))
@@ -145,11 +108,4 @@ def get_model():
 
 
 if __name__ == "__main__":
-
-    for j in range(1, 201, 1):
-        UNITS = j
-        i = 0
-        while i < 1:
-            main()
-            i += 1
-
+    main()
